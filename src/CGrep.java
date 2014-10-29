b46ad929c06338097c4ea416d15eff32efe63b25
kocsen <kocsenc@gmail.com>
2014-10-29 13:58:46 -0400

    Update to Cgrep, added ^ and $

--- src/CGrep.java
+++ src/CGrep.java
@@ -1,6 +1,4 @@
 import java.io.Console;
-import java.io.File;
-import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.util.ArrayList;
 import java.util.regex.Pattern;
@@ -13,22 +11,21 @@ import java.util.regex.Pattern;
 
 public class CGrep {
 
-	/**
-	 * @param args
-	 * @throws FileNotFoundException 
-	 */
-	public static void main(String[] args) throws FileNotFoundException {
-		Pattern pattern = Pattern.compile(args[0]);
-		if ( args.length == 1 ) {
-			String input;
-			Console console = System.console();
-			input = console.readLine("Enter input:");
-		}
-		else {
-			ArrayList<String> files = new ArrayList<String>();
-			for ( int c = 1; c < args.length; c++ ) {
-				files.add(args[c]);
-			}
-		}
-	}
+    /**
+     * @param args
+     * @throws FileNotFoundException
+     */
+    public static void main(String[] args) throws FileNotFoundException {
+        Pattern pattern = Pattern.compile(String.format("^%s$", args[0]));
+        if (args.length == 1) {
+            String input;
+            Console console = System.console();
+            input = console.readLine("Enter input:");
+        } else {
+            ArrayList<String> files = new ArrayList<String>();
+            for (int c = 1; c < args.length; c++) {
+                files.add(args[c]);
+            }
+        }
+    }
 }
